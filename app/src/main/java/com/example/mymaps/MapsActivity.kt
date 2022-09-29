c456f5fda7c7ccc0a4f327325bea6f45b4b84d39
Arvin-k17 <kunwararvind1317@gmail.com>
2022-09-30 00:59:18 +0530

    first commit

---
+++ app/src/main/java/com/example/mymaps/MapsActivity.kt
@@ -0,0 +1,66 @@
+package com.example.mymaps
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.example.mymaps.databinding.ActivityMapsBinding
+import com.example.mymaps.models.UserMap
+import com.google.android.gms.maps.model.LatLngBounds
+
+private const val TAG = "MapsActivity"
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+    private lateinit var binding: ActivityMapsBinding
+    private lateinit var userMap : UserMap
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMapsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        userMap = intent.getSerializableExtra(EXTRA_USER_MAP) as UserMap
+
+        supportActionBar?.title = userMap.title
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        Log.i(TAG,"user map to render: ${userMap.title}")
+
+        val boundsBuilder = LatLngBounds.Builder()
+        for(place in userMap.places){
+            val latlng = place?.let { LatLng(it.latitude, place.longitude) }
+            latlng?.let { boundsBuilder.include(it) }
+            latlng?.let { MarkerOptions().position(it).title(place.title).snippet(place.description) }
+                ?.let { mMap.addMarker(it) }
+        }
+
+//        mMap.moveCamera(CameraUpdateFactory.newLatLngBounds(boundsBuilder.build(),1000,1000,0))
+        mMap.animateCamera(CameraUpdateFactory.newLatLngBounds(boundsBuilder.build(),1000,1000,0))
+
+    }
+}
